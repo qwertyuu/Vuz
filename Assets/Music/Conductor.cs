e4beaf861c4de1ee218d61988e31c5c51626ad38
Raphaël Côté <cotlarrc@msn.com>
2019-05-19 16:53:17 -0400

    Initial POC commit

---
+++ Assets/Music/Conductor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : SceneSingleton<Conductor>
+{
+    event System.Action<decimal, decimal> _onBeat = delegate { };
+    public static event System.Action<decimal, decimal> OnBeat
+    {
+        add { Instance._onBeat += value; }
+        remove { Instance._onBeat -= value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 beatScale = new Vector3(2f, 2f, 2f);
+    Vector3 normalScale = new Vector3(1f, 1f, 1f);
+    float t = 0;
+    float speed = 0.1f;
+
+
+    void Start()
+    {
+        AudioProcessor.OnBeat += a;
+    }
+
+    void a()
+    {
+        t = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t = Mathf.Min(speed + t, 1);
+
+        transform.localScale = Vector3.Lerp(beatScale,normalScale , t);
+
+    }
+}
